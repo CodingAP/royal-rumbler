16cc1a3f52be7f73d1807bad51a700bb33473667
Alex Prosser <apsoutherngaming07@gmail.com>
2024-07-24 15:43:14 -0500

    Uploading local code

--- server.js
+++ server.js
@@ -1,12 +1,48 @@
 import express from 'express';
-const app = express();
+import { readFileSync, existsSync } from 'fs';
+import { parse } from 'path';
 
+const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+const routes = {
+    '/': 'public/index.html'
+}
+
+const contentTypes = {
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.css': 'text/css',
+    '.png': 'image/png',
+    '.jpg': 'image/jpeg',
+    '.jpeg': 'image/jpeg',
+}
+
+app.get('*', (request, response) => {
+    let filePath = routes[request.path] || routes[request.path + '.html'];
+    let content, contentType = 'text/plain';
+    if (filePath !== undefined) {
+        // The request is for a defined route
+        content = readFileSync(filePath).toString();
+        contentType = 'text/html';
+    } else {
+        filePath = 'public' + request.path;
+        if (existsSync(filePath)) {
+            // The request is for a public file
+            content = readFileSync(filePath).toString();
+            if (contentTypes[parse(filePath).ext] !== undefined) {
+                contentType = contentTypes[parse(filePath).ext];
+            }
+        } else {
+            // The request is for a file not found
+            content = readFileSync('public/404.html').toString();
+            contentType = 'text/html';
+        }
+    }
+
+    response.contentType(contentType).send(content);
 });
 
 app.listen(PORT, () => {
-    console.log(`Royal Rumbler app listening on port ${PORT}`)
+    console.log(`App listening on port ${PORT}`);
 });
